fd7a381bdceb312a52afe5887a288d7e279e7b31
cmack29 <conor.m@me.com>
2021-07-16 13:22:52 +0100

    Task completed

--- index.js
+++ index.js
@@ -5,15 +5,55 @@ for (let i=0; i < users.length; i++) {
     const name = users[i].name;
     const city = users[i].address.city;
 
-    const row = `${id} | ${name} ${city}\n`;
+    const row = `${id}\n | ${name}\n ${city}\n`;
 
     userInfo = userInfo + row;
 
+    console.log("Iteration #", i + 1);
+    console.log("message: ", userInfo);
+
     console.log(userInfo);
 };
+
+console.log(userInfo);
 alert(userInfo);
 
+const selectedUserId = prompt("Enter your ID to show your list");
 
-// console.log("users: ", users);
+let theUser = "";
 
-// console.log("todos: ", todos);
+for (let i=0; i < users.length; i++) {
+  
+    if (parseInt(selectedUserId) === users[i].id) {
+        const username = users[i].username
+        console.log(username);
+        theUser = username + `\n`;
+    }
+}
+
+console.log(theUser); 
+
+let todo = "";
+
+for (let i = 0; i < todos.length; i++) {
+    
+    const userId = todos[i].userId;
+    const title = todos[i].title;
+    const completed = todos[i].completed;
+    
+    const list = `${userId}\n | ${title}\n | ${completed}\n`;
+    console.log(typeof selectedUserId, typeof userId);
+
+
+    if (parseInt(selectedUserId) === userId) {
+        todo = todo + list;
+
+        // console.log("selected user ID: ", selectedUserId);
+    }
+
+    // console.log("Iteration #", i + 1);
+    // console.log("To do: ", todo);
+}
+const fulltodo = theUser + todo;
+console.log(fulltodo);
+alert(fulltodo);
